4ea03a5db32416106d71f9e6501c656d2279b665
Katia-ST <katia.tanimoto@gmail.com>
2021-05-18 22:30:08 -0300

    first commit Netflix clone with button features

---
+++ js/owl/setup.js
@@ -0,0 +1,18 @@
+//$ é o JQuery, que é uma ferramenta para manipular elementos em tela no JS
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
